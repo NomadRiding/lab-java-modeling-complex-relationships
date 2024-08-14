0066da50a091074d2250e2faf7d02ed716219378
nomadriding <fundora.a@gmail.com>
2024-08-14 11:07:05 -0400

    build springboot project

---
+++ modeling/src/test/java/com/ironhack/modeling/ModelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.modeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
